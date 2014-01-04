8fb14c578e79b8e6e1e8dd684026543e82f40a3b
Travis Gerrard <travisgerrard@gmail.com>
2014-01-04 13:44:52 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user # Method found in session_helper
 		else
 			# Create an error message and reload signin page
 			flash.now[:error] = 'Invalid email/password combination' # Almost there
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,9 +20,25 @@ module SessionsHelper
 		@current_user ||= User.find_by(remember_token: remember_token)
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
 	def sign_out
 		current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
 		cookies.delete(:remember_token)
 		self.current_user = nil
+	end
+
+	# Used in session controller, to direct to proper page after signing in!
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	# Puts requested URL in session variable under key :return_to 
+	# -For when user tries to go to part of site that needs to be logged into, after logging in gets forwarded to desired location
+	def store_location
+		session[:return_to] = request.url if request.get?
 	end
 end
